b7722452ab58fc572732d739068bcd4b64546688
Unknown <rpramudito@gmail.com>
2017-12-12 22:43:05 +0700

    Auto stash before merge of "master" and "origin/master"
    Radit

---
+++ app/src/main/java/onex7/belajarpapb/bab9.java
@@ -0,0 +1,23 @@
+package onex7.belajarpapb;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by RHP on 12-Dec-17.
+ */
+
+public class bab9 extends Fragment{
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.bab9, container, false);
+        return myView;
+    }
+}
