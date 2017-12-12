017d6c2f8a73c6874021e9b826dcca8022177fee
Unknown <zulfikarfaras@gmail.com>
2017-12-12 22:33:53 +0700

    Zul

---
+++ app/src/main/java/onex7/belajarpapb/bab6.java
@@ -0,0 +1,23 @@
+package onex7.belajarpapb;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by FIKAR on 12/12/2017.
+ */
+
+public class bab6 extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.bab6,container,false);
+        return myView;
+    }
+}
